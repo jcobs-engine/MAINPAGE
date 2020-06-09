10ca08d0f9d0735f0d9f9c56126b8b87a294faee
Jcobs-Engine <50663715+jcobs-engine@users.noreply.github.com>
2020-06-09 12:53:54 +0200

    v1

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+echo '<html><head><meta http-equiv="refresh" content="1; URL=main.php"><link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"></head><body style="background-color:black;color:#00ff00;font-family:Source Code Pro">Redirect...</body></html>';
+?>
