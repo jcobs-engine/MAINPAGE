62a3eb65f40d78810fcf9bd1e8700725db7b0b4e
Jcobs-Engine <50663715+jcobs-engine@users.noreply.github.com>
2020-07-04 08:27:09 +0200

    v1

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
-echo '<html><head><meta http-equiv="refresh" content="1; URL=main.php"><link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"></head><body style="background-color:black;color:#00ff00;font-family:Source Code Pro">Redirect...</body></html>';
+echo '
+<html>
+<head>
+<title>MainPage - A Free and Anonymous Social Network!</title>
+<meta name="description" content="A free, open source and anonymous social network. Create and read blogs, forums and chats, or participate in votings. Be a part of the community!">
+<meta name="keywords" content="mainpage, social network, network, anonymous, community, blogs, blog, forum, forums, chat, chats, votings">
+<link rel="shortcut icon" type="image/x-icon" href="DATA/icon.ico">
+<meta charset="utf-8">
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+
+<link href="font.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
+
+<meta http-equiv="refresh" content="1; URL=main.php">
+
+</head>
+<body style="background-color:black;color:#00ff00;font-family:Source Code Pro">Redirect...</body>
+</html>';
 ?>
